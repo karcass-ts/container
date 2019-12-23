a03ca65844e2a13f240a18d4665d6b02f8cec0f7
Valery Shibanov <shibaon@gmail.com>
2019-12-23 17:50:37 +0300

    Detection of recursion considering async ioc getter

--- src/Container.ts
+++ src/Container.ts
@@ -4,7 +4,7 @@ export class Container<ItemType extends any> {
         key: (new (...args: any[]) => ItemType)|string,
         initializer: () => ItemType|Promise<ItemType>,
         instance?: any,
-        initializationsCount: number
+        initializersCallStack: string[],
     }> = {}
 
     public add<T extends ItemType>(key: (new (...args: any[]) => T)|string, initializer: () => T|Promise<T>) {
@@ -12,7 +12,7 @@ export class Container<ItemType extends any> {
         if (name in this.items) {
             throw new Error(`The item with name "${name}" already added before`)
         }
-        this.items[name] = { key, initializer, initializationsCount: 0 }
+        this.items[name] = { key, initializer, initializersCallStack: [] }
     }
 
     public async addInplace<T extends ItemType>(key: (new (...args: any[]) => T)|string, initializer: () => T|Promise<T>): Promise<T> {
@@ -26,15 +26,34 @@ export class Container<ItemType extends any> {
         if (!item) {
             throw new Error(`Item with key "${name}" not found`)
         }
-        if (!item.instance) {
-            if (item.initializationsCount > 0) {
-                throw new Error(`Circular dependency in ${name} initializer detected`)
+        const checkRecursion = () => {
+            const stack = new Error().stack
+            if (!stack) {
+                throw new Error('Unable to receive callstack')
             }
-            item.initializationsCount++
+            for (const line of stack.split('\n').filter(line => line.indexOf('at Object.initializer') >= 0)) {
+                const recursive = Object.values(this.items).find(otherItem => {
+                    if (otherItem === item) {
+                        return false
+                    }
+                    return otherItem.initializersCallStack.find(l => l === line)
+                })
+                if (recursive) {
+                    const recName = typeof recursive.key === 'string' ? recursive.key : recursive.key.name
+                    throw new Error(`Circular dependency ${recName} <==> ${name} detected`)
+                }
+                item.initializersCallStack.push(line)
+            }
+        }
+        if (!item.instance) {
+            checkRecursion()
             item.instance = item.initializer()
+            if (item.instance instanceof Promise) {
+                item.instance.then(result => item.instance = result).catch(() => item.instance = undefined)
+            }
         }
         if (item.instance instanceof Promise) {
-            item.instance = await item.instance
+            checkRecursion()
         }
         return item.instance
     }
--- src/index.test.ts
+++ src/index.test.ts
@@ -24,11 +24,17 @@ class NeverClass {}
 
 const container = new Container()
 it('Order must be empty', () => {
-    container.add(MasterClass, async () => new MasterClass(await container.get(SlaveClass)))
+    container.add(MasterClass, async () => {
+        await new Promise(resolve => setTimeout(resolve, 100))
+        return new MasterClass(await container.get(SlaveClass))
+    })
     container.add(SlaveClass, () => new SlaveClass())
     assert.deepEqual(records, [])
 })
 it(`Order must be ${SlaveClass.name}, ${MasterClass.name}`, async () => {
+    container.get(MasterClass)
+    container.get(MasterClass)
+    container.get(MasterClass)
     await container.get(MasterClass)
     assert.deepEqual(records, [SlaveClass.name, MasterClass.name])
 })
@@ -41,15 +47,18 @@ it(`Initialization order must be ${InlineClass.name}`, () => {
     container.addInplace(InlineClass, () => new InlineClass())
     assert.deepEqual(records, [InlineClass.name])
 })
-it('Must throw circular dependency error', async () => {
+it('Must throw circular dependency error async', async () => {
     class LeftSide { public constructor(protected rightSide: RightSide) {} }
     class RightSide { public constructor(protected leftSide: LeftSide) {} }
-    container.add(LeftSide, async () => new LeftSide(await container.get(RightSide)))
+    container.add(LeftSide, async () => {
+        await new Promise(resolve => setTimeout(resolve, 10))
+        return new LeftSide(await container.get(RightSide))
+    })
     container.add(RightSide, async () => new RightSide(await container.get(LeftSide)))
     try {
         await container.get(LeftSide)
     } catch (err) {
-        assert(err.message.indexOf('detected'))
+        assert(err.message.indexOf('Circular dependency') >= 0)
         return
     }
     assert.fail('Error not throwed')
